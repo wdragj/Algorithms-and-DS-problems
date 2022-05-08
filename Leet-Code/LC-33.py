69a94bfc409c55daec525388048ac6fa466f3622
Freddy Seo <wdragj@gmail.com>
2022-05-08 12:23:12 +0900

    2022/05/08

---
+++ Leet-Code/LC-33.py
@@ -0,0 +1,27 @@
+# Medium
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if target == nums[mid]:
+                return mid
+            
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else: # target <= nums[mid] or target >= nums[l]
+                    r = mid - 1
+
+            # right sorted portion
+            else: # nums[r] >= nums[mid]
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else: # target >= nums[mid] or target <= nums[r]
+                    l = mid + 1
+        
+        return -1
