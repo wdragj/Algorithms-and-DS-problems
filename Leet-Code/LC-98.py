bdf8aeb9d59051945c91d3a2122182696c53972e
Freddy Seo <wdragj@gmail.com>
2022-05-14 16:32:36 +0900

    2022/05/14

--- Leet-Code/LC-98.py
+++ Leet-Code/LC-98.py
@@ -10,17 +10,16 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         # 1. Left subtree is less than node AND Right subtree is greater than node (BST)
         # 2. Left and right subtree must also be BST (Lbst, Rbst)
-        
-        
-        def valid(node, left, right): # node, left boundary, right boundary
-            if not node: # If node is null, True
+
+        def valid(node, left, right):  # node, left boundary, right boundary
+            if not node:  # If node is null, True
                 return True
-            
+
             # if node is NOT less than right boundary and greater than left boundary, False
             if not(node.val < right and node.val > left):
                 return False
-            
+
             return valid(node.left, left, node.val) and valid(node.right, node.val, right)
-        
+
         # We don't know the value of root so set its left boundary to -infinity and right to infinity
         return valid(root, float("-inf"), float("inf"))
